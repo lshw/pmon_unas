391f52b130e249c609ac36e8f4f0efcea25ba73f
liu shiwei <liushiwei@gmail.com>
2016-08-27 22:35:32 +0800

    增加烧录说明

--- build_2fnas.sh
+++ build_2fnas.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+#本脚本在debian5-debian9测试没有问题
 if ! [ -x /usr/local/comp ]  ; then
 wget http://mirrors.ustc.edu.cn/loongson/pmon/toolchain-pmon.tar.bz2 -c
 tar jxvf toolchain-pmon.tar.bz2
 mv usr/local/comp /usr/local/comp
-#rm -rf usr  
+rm -rf usr  
 fi
 
 PATH=/usr/local/comp/mips-elf/gcc-2.95.3/bin:$PATH
 cd zloader.2fnas
-make cfg all tgt=ram
+make cfg
+make all tgt=ram
 cp gzram ../gzram.2fnas
-make cfg all tgt=rom
+make all tgt=rom
 cp gzrom.bin ../pmon_2fnas.bin
